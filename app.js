a5a70d081ab047364414134e06a04098b4c9b1ae
zenius <zenius2050@gmail.com>
2019-06-20 21:07:49 +0530

    debugging npm module added

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require("express");
 var chalk = require('chalk'); 
+var debug = require('debug')('app'); 
+var morgan = require('morgan'); 
 
 var app = express(); 
 var port = process.env.PORT || 3000; 
+
+// app.use(morgan('combined')); 
+app.use(morgan('tiny')); 
 
 app.get("/", function(req, res){ 
     res.send("Welcome to Express"); 
 }); 
 
 app.listen(port, function() { 
-    console.log(`server listening on port ${chalk.green(port)}`); 
+    debug(`server listening on port ${chalk.green(port)}`); 
 }); 
 
