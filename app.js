c9150d4ece0b781d4fb07e39182a035d7224d60d
zenius <zenius2050@gmail.com>
2019-06-20 21:21:14 +0530

    index.html file added

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require("express");
 var chalk = require('chalk'); 
 var debug = require('debug')('app'); 
 var morgan = require('morgan'); 
+var path = require('path'); 
 
 var app = express(); 
 var port = process.env.PORT || 3000; 
 
-// app.use(morgan('combined')); 
 app.use(morgan('tiny')); 
 
 app.get("/", function(req, res){ 
-    res.send("Welcome to Express"); 
+    res.sendFile(path.join(__dirname, 'views/index.html')); 
 }); 
 
 app.listen(port, function() { 
