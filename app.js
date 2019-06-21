c8ee3d4fe3f29e2f0cb4fd0aabfd879a7fe6cee3
zenius <zenius2050@gmail.com>
2019-06-22 03:15:48 +0530

    static files served through node modules - bootstrap and jquery

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ var app = express();
 var port = process.env.PORT || 3000; 
 
 app.use(morgan('tiny')); 
+// serving static files - public directory 
 app.use(express.static(path.join(__dirname, 'public'))); 
+
+// serving static files - node modules
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 app.get("/", function(req, res){ 
     res.sendFile(path.join(__dirname, 'views/index.html')); 
