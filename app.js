e043cac8731d43dcb69e27827f94509b2c85a5c2
zenius <zenius2050@gmail.com>
2019-06-22 05:17:37 +0530

    tooling options added - eslint, nodemon and npm scripts

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-var express = require("express");
-var chalk = require('chalk'); 
-var debug = require('debug')('app'); 
-var morgan = require('morgan'); 
-var path = require('path'); 
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express(); 
-var port = process.env.PORT || 3000; 
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(morgan('tiny')); 
-// serving static files - public directory 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(morgan('tiny'));
+// serving static files - public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // serving static files - node modules
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
-app.get("/", function(req, res){ 
-    res.sendFile(path.join(__dirname, 'views/index.html')); 
-}); 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-app.listen(port, function() { 
-    debug(`server listening on port ${chalk.green(port)}`); 
-}); 
-
+app.listen(port, () => {
+  debug(`server listening on port ${chalk.green(port)}`);
+});
