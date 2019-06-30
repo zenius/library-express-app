4dca9d6cb11f81f0b1cd54b826dd30ccd684a298
zenius <zenius2050@gmail.com>
2019-06-30 19:10:02 +0530

    setup for passport npm packages added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const nav = [
   { link: '/books', title: 'Books' },
@@ -23,6 +25,11 @@ app.use(morgan('tiny'));
 // body parser for Post methods
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// cookie-parser and expression-session
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
 
 // serving static files - public directory
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ src/config/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+
+function passportConfig(app) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // stores User in session
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  // Retrieves User from session
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  return passport;
+}
+
+module.exports = passportConfig;
